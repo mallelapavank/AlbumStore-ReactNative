c5ea0cb92bcef39cb6864ceaaa1f196789b910a6
PavanMallela <mallelapavank@gmail.com>
2017-08-01 11:02:48 +0530

    initial commit

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    headerTextContainer,
+    headerTitleText,
+    thumbnailStyle,
+    thumbnailContainer,
+    imageStyle
+  } = styles;
+  return (
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainer}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerTextContainer}>
+          <Text style={headerTitleText}>
+            {title}
+          </Text>
+          <Text>
+            {artist}
+          </Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+      <CardSection>
+        <Button whenPressed={console.log('Pressed')} />
+      </CardSection>
+    </Card>
+  );
+};
+const styles = {
+  headerTextContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  headerTitleText: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+};
+
+export default AlbumDetail;
